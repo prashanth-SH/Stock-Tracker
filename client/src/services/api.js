00f47bda547bd5fc59ad05408709018f6f08c2bf
prashanth-SH <aprashanth826@gmail.com>
2026-05-27 14:55:16 +0530

    fixed production backend api url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  // baseURL: "http://localhost:5000/api"
+  baseURL: "https://stock-tracker-z8w0.onrender.com/api"
 });
 
 // Attach JWT automatically
